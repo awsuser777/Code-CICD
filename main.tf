04025929c104b4c74df4a4254e697e02e8dcc833
awsuser777 <146702459+awsuser777@users.noreply.github.com>
2023-12-12 12:56:06 +0530

    Future 1 merge into main (#1)
    * Adding new line in future1 branch.
    
    * new commit from future_1

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ resources "aws_instances" "Test-server" {
  ami = "shhqdhijxkasmxkojxk"
  instances_type = t2.micro
   key = "test_key"
-}
+}  
+
+# this is future_1 brabch created inside the main branch.
+
